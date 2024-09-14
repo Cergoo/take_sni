2a6e6fdb0d7f1fd9a6bfc1fb990477465b2cba8c
Cergoo <itcraft.letter@gmail.com>
2024-09-14 19:26:38 +0500

    new resion

--- src/lib.rs
+++ src/lib.rs
@@ -7,12 +7,16 @@ use log;
 /// take sni from buffer and return Option
 pub fn take_sni(b: &[u8]) -> Option<(usize, &[u8])> {
     inner_take_sni(b).ok()
-} 
+}
+
+// take just coordinat sni in buffer
+pub fn take_sni_point(b: &[u8]) -> Option<(usize, usize)> {
+    inner_take_sni(b).ok().map(|r|(r.0-r.1.len(), r.0))
+}
 
 /// take sni from buffer and return Result
 pub fn inner_take_sni(b: &[u8]) -> Result<(usize, &[u8]), &[u8]> {
-    use byteorder::{ByteOrder, BE}; 
-    use parcelona::{parser_combinators::{*}, u8::is_any};
+    use parcelona::u8ext::{*};
     
     const HANDSHAKE_TYPE_CLIENT_HELLO: usize = 1;
     const EXTENSION_TYPE_SNI: usize = 0;
@@ -20,58 +24,55 @@ pub fn inner_take_sni(b: &[u8]) -> Result<(usize, &[u8]), &[u8]> {
     
     let origin_len = b.len(); 
     if origin_len < 10 { return Err(b); }
-    let take1 = map(take(seq(is_any,SeqCount::Exact(1))),|x|{x[0] as usize});    
-    let take2 = map(take(seq(is_any,SeqCount::Exact(2))),|x|{BE::read_u16(x) as usize});    
-    let take3 = map(take(seq(is_any,SeqCount::Exact(3))),|x|{BE::read_u24(x) as usize}); 
-
 
     let b = &b[5..]; 
     // Handshake message type.
-    let (b, c) = take1.parse(b)?;
+    let (b, c) = take_len_be_u8(b)?;
     if c!= HANDSHAKE_TYPE_CLIENT_HELLO { return Err(b"Er"); } 
     
     // Handshake message length.
-    let (b, c) = take3.parse(b)?;
+    let (b, c) = take_len_be_u24(b)?;
     log::debug!("1. messag len {:?}", c);
     
     // ProtocolVersion (2 bytes) & random (32 bytes).
-    let (b, c) = take(seq(is_any,SeqCount::Exact(34))).parse(b)?;
+    let (b, _) = take_record(b,34)?;
     
     // Session ID (u8-length vec), cipher suites (u16-length vec), compression methods (u8-length vec).
-    let (b, c) = take1.parse(b)?;
-    log::debug!("2. session id (need 0) {:?}", c);
-    let (b, c) = take(seq(is_any,SeqCount::Exact(c))).parse(b)?;
-    let (b, c) = take2.parse(b)?;
-    let (b, c) = take(seq(is_any,SeqCount::Exact(c))).parse(b)?;
-    let (b, c) = take1.parse(b)?;
-    let (b, c) = take(seq(is_any,SeqCount::Exact(c))).parse(b)?;
-    
+    let (b, _) = take_record_be_u8(b)?;
+    let (b, _) = take_record_be_u16(b)?;
+    let (b, _) = take_record_be_u8(b)?;
+
     // Extensions length.
-    let (mut b, mut c) = take2.parse(b)?;
+    let (mut b, mut c) = take_len_be_u16(b)?;
+    let mut ext_type: usize;
+    let mut ext_leng: usize;
     log::debug!("3. Extensions length {:?}", c);
-    let (mut ext_type, mut ext_leng) = (0_usize, 0_usize);
     loop {
         // Extension type & length.
-        (b, (ext_type, ext_leng)) = pair(take2.clone(), take2.clone()).parse(b)?;
+        (b, ext_type) = take_len_be_u16(b)?;
+        (b, ext_leng) = take_len_be_u16(b)?;
+
         log::debug!("4. Ext type (0) {:?} len {:?}", ext_type, ext_leng);
         if ext_type != EXTENSION_TYPE_SNI {
-            if ext_leng>0 { (b, _) = take(seq(is_any,SeqCount::Exact(ext_leng))).parse(b)?; }
+            if ext_leng>0 { (b, _) = take_record(b, ext_leng)?; }
             continue; 
         }
         // ServerNameList length.
-        (b, c) = take2.parse(b)?;
+        (b, c) = take_len_be_u16(b)?;
         log::debug!("5. ServerNameListmessag len {:?}", c);
         // ServerNameList.
-        let (mut name_type, mut name_leng) = (0_usize, 0_usize);
         let mut sni: &[u8];
+        let mut name_type: usize;
+        let mut name_leng: usize;
         loop {
             // NameType & length.
-            (b, (name_type, name_leng)) = pair(take1.clone(), take2.clone()).parse(b)?;
-            (b, sni) = take(seq(is_any,SeqCount::Exact(name_leng))).parse(b)?;
+            (b, name_type) = take_len_be_u8(b)?;
+            (b, name_leng) = take_len_be_u16(b)?;
+            (b, sni) = take_record(b, name_leng)?;
             if name_type != NAME_TYPE_HOST_NAME { continue; }
-            name_leng = origin_len-b.len();
-            log::info!("[sni] {:?} sni {:?}", name_leng, String::from_utf8_lossy(sni));
-            return Ok((name_leng, sni));
+            let sni_point: usize = origin_len-b.len();
+            log::info!("[sni] {:?} sni {:?}", sni_point, String::from_utf8_lossy(sni));
+            return Ok((sni_point, sni));
         }  
     }
 }
